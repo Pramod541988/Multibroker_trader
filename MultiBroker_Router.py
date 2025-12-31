015fee943b9677d92e6f2e97e76d73f0cd1d4c62
Pramod541988 <125286455+Pramod541988@users.noreply.github.com>
2025-12-31 10:20:50 +0530

    Update MultiBroker_Router.py

--- MultiBroker_Router.py
+++ MultiBroker_Router.py
@@ -53,6 +53,7 @@ app.add_middleware(
     allow_origins=[
         "https://multibrokertrader-production.up.railway.app",
         "https://multibroker-trader.onrender.com",
+        "https://multibroker-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -2000,6 +2001,7 @@ def route_modify_order(payload: Dict[str, Any] = Body(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("MultiBroker_Router:app", host="127.0.0.1", port=5001, reload=False)
+
 
 
 
